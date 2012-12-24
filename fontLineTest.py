81d7c64c285551fb16dd4a52fc6b5ebba304a05b
Matthew Lewis <matt@mplewis.com>
2012-12-23 22:02:07 -0600

    added an experimental screen
    working font and screen demo

--- fontLineTest.py
+++ fontLineTest.py
@@ -1,1 +1,53 @@
 #!/usr/bin/python2
+
+import fontFirst
+
+font = fontFirst.fontFirst
+
+class LedScreen:
+	def __init__(self, lines):
+		self.lines = lines
+		self.screen = []
+		for n in range(lines):
+			self.screen.append('')
+	def __repr__(self):
+		return 'LED screen: ' + str(self.lines) + ' lines, ' + str(self.getWidth()) + ' px wide'
+	def clear(self):
+		for n in range(lines):
+			self.screen[n] = ''
+	def display(self):
+		return self.screen
+	def getWidth(self):
+		return len(self.screen[0])
+	def addChar(self, pxList):
+		charLines = len(pxList)
+		if charLines > self.lines:
+			raise ValueError('Too many lines in character.')
+		charWidth = 0
+		for line in pxList:
+			if len(line) > charWidth:
+				charWidth = len(line)
+		print self.getWidth()
+		if self.getWidth() != 0:
+			print 'Width = ' + str(self.getWidth())
+			for n in range(self.lines):
+				self.screen[n] += ' '
+		for n in range(self.lines):
+			if n < len(pxList):
+				toAdd = pxList[n]
+			else:
+				toAdd = ''
+			while len(toAdd) < charWidth:
+				toAdd += ' '
+			self.screen[n] += toAdd
+
+screen = LedScreen(8)
+
+text = 'Dead Beef Cafe'
+for char in text:
+	screen.addChar(font[char])
+
+print screen
+scrLines = screen.display()
+for line in scrLines:
+	print repr(line)
